1edef474d108dfd86a4fa63e3c5d25d478923eb6
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-13 16:54:25 +0400

    1.Background image

--- app/src/main/java/com/weathercompose/screens/MainScreen.kt
+++ app/src/main/java/com/weathercompose/screens/MainScreen.kt
@@ -1,9 +1,27 @@
 package com.weathercompose.screens
 
+
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.weathercompose.R
+
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreen() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_back_new),
+        contentDescription = "imageBack",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds
+    )
+
 }
