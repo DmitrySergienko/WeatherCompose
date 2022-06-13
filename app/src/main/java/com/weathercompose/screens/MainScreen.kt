9897a477ea1eaa648cff0e6f6e57989ad0511930
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-13 18:01:16 +0400

    2.First card with date and image (icon form api) installed

--- app/src/main/java/com/weathercompose/screens/MainScreen.kt
+++ app/src/main/java/com/weathercompose/screens/MainScreen.kt
@@ -3,13 +3,23 @@ package com.weathercompose.screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.weathercompose.R
+import com.weathercompose.ui.theme.BlueLight
 
 
 @Preview(showBackground = true)
@@ -23,5 +33,43 @@ fun MainScreen() {
             .alpha(0.5f),
         contentScale = ContentScale.FillBounds
     )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().alpha(0.7f),
+            backgroundColor = BlueLight,
+            elevation = 0.dp,
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "20 July 2022",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/113.png",
+                        contentDescription = "imageIcon",
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(top = 8.dp, end = 80.dp)
+                    )
+
+                }
+            }
+        }
+
+    }
 
 }
